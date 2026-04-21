82dc38f9817da0458023798c60cdb5e62d26b952
Victory's create <joelvictory62@gmail.com>
2026-04-21 13:15:25 +0100

    Create main application file

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+from routes import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
